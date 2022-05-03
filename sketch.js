da45459982ae6ae5f099f42d06ba1f5acee8cfbc
Baiano-Indiano <99291603+Baiano-Indiano@users.noreply.github.com>
2022-05-03 20:32:28 -0300

    03/05/2022

--- sketch.js
+++ sketch.js
@@ -16,11 +16,28 @@ var fruit;
 
 var stick;
 
+var bunnySprite;
+
+var backgroundImage, fruitImage, rabbitImage,buttonImage;
+
+function preload(){
+  backgroundImage = loadImage("./preload/background.png");
+  fruitImage = loadImage("./preload/melon.png");
+  rabbitImage = loadImage("./preload/Rabbit-01.png");
+}
+
 function setup() 
 {
   createCanvas(500,700);
   engine = Engine.create();
   world = engine.world;
+  bunnySprite = createSprite(250,650,100,100);
+  bunnySprite.scale = 0.2;
+  bunnySprite.addImage(rabbitImage);
+  buttonImage = createImg("./preload/cut_button.png");
+  buttonImage.position(220,30);
+  buttonImage.size(30,30);
+  buttonImage.mouseClicked(cortar);
  
   floor = new Floor(200,690,600,20);
   rope = new Rope(6,{x:245,y:30});
@@ -30,18 +47,25 @@ function setup()
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  imageMode(CENTER);
   textSize(50)
 }
 
 function draw() 
 {
   background(51);
+  image(backgroundImage, width/2, height/2, 500, 700);
   Engine.update(engine);
-  ellipse(fruit.position.x, fruit.position.y, 15,15);
+  image(fruitImage,fruit.position.x, fruit.position.y, 65,65);
   floor.show();
   rope.show();
+  drawSprites();
 }
 
-
+function cortar(){
+  rope.break();
+  stick.noConnect();
+  stick = null;
+}
 
 
