f8f32fb6cf312c101e9df8a4fb6bac5a8e3eb55b
Baiano-Indiano <99291603+Baiano-Indiano@users.noreply.github.com>
2022-05-05 20:43:11 -0300

    05/05/2022

--- sketch.js
+++ sketch.js
@@ -20,10 +20,22 @@ var bunnySprite;
 
 var backgroundImage, fruitImage, rabbitImage,buttonImage;
 
+var blink, eating, sad;
+
 function preload(){
   backgroundImage = loadImage("./preload/background.png");
   fruitImage = loadImage("./preload/melon.png");
   rabbitImage = loadImage("./preload/Rabbit-01.png");
+  blink = loadAnimation("./preload/blink_1.png","./preload/blink_2.png","./preload/blink_3.png");
+  eating = loadAnimation("./preload/eat_0.png","./preload/eat_1.png","./preload/eat_2.png","./preload/eat_3.png","./preload/eat_4.png");
+  sad = loadAnimation("./preload/sad_1.png","./preload/sad_2.png","./preload/sad_3.png");
+
+  blink.playing = true;
+  eating.playing = true;
+  sad.playing = true;
+
+  eating.looping = false;
+  sad.looping = false;
 }
 
 function setup() 
@@ -31,9 +43,15 @@ function setup()
   createCanvas(500,700);
   engine = Engine.create();
   world = engine.world;
-  bunnySprite = createSprite(250,650,100,100);
+  bunnySprite = createSprite(270,620,100,100);
   bunnySprite.scale = 0.2;
   bunnySprite.addImage(rabbitImage);
+  bunnySprite.addAnimation("blink", blink);
+  bunnySprite.addAnimation("eating", eating);
+  bunnySprite.addAnimation("sad", sad);
+
+  bunnySprite.changeAnimation("blink");
+
   buttonImage = createImg("./preload/cut_button.png");
   buttonImage.position(220,30);
   buttonImage.size(30,30);
@@ -44,6 +62,9 @@ function setup()
   fruit = Bodies.circle(300,300,15);
   World.add(world,fruit);
   stick = new Connect(rope,fruit);
+
+  blink.frameDelay = 30;
+  eating.frameDelay = 20;
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -56,9 +77,20 @@ function draw()
   background(51);
   image(backgroundImage, width/2, height/2, 500, 700);
   Engine.update(engine);
-  image(fruitImage,fruit.position.x, fruit.position.y, 65,65);
+
+  if(fruit !== null){
+    image(fruitImage,fruit.position.x, fruit.position.y, 65,65);
+  }
+ 
   floor.show();
   rope.show();
+  if(collision(fruit,bunnySprite) === true){
+    bunnySprite.changeAnimation("eating");
+  }
+  if(fruit !== null && fruit.position.y >= 650){
+    bunnySprite.changeAnimation("sad");
+    fruit = null;
+  }
   drawSprites();
 }
 
@@ -68,4 +100,15 @@ function cortar(){
   stick = null;
 }
 
-
+function collision(corpo_1,sprite_1){
+  if(corpo_1 !== null){
+    var meters = dist(corpo_1.position.x, corpo_1.position.y, sprite_1.position.x, sprite_1.position.y);
+    if(meters <= 80 ){
+      World.remove(world,fruit);
+      fruit = null;
+      return true; 
+    } else{
+      return false;
+    }
+  }
+}
